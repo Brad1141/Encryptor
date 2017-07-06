097d83a1f78a554adf45d5900c42fd981565094e
Brad1141 <bradlamontagne1141@gmail.com>
2017-07-06 09:52:34 -0400

    Add files via upload

---
+++ Fix  this(temperature converter).cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your Name?");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Hello {0}, how are you today? ", Name);
+            string emotion = Console.ReadLine();
+            Console.WriteLine("That is Wonderfull!, How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Cool! I am 17 years old, that means I am {0} older than you", 17 - age);
+            Console.ReadLine();
+        }
+    }
+}
