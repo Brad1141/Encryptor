5121a25da3dadfbccd020acca694efa422ab41ab
Brad LaMontagne <bradlamontagne1141@gmail.com>
2017-07-22 15:34:41 -0400

    added button to motor code

--- Motor Code.ino
+++ Motor Code.ino
@@ -1,10 +1,13 @@
+const int button = 2;
 
 void setup() {
+  pinMode(2, INPUT);
   pinMode(12, OUTPUT); //Direction of Motor
   pinMode(9, OUTPUT); //Brake of Motor
 }
 
 void loop() {
+  if (button == HIGH) {
   //forward at full speed
   digitalWrite(12, HIGH); //Direction is set to forward
   digitalWrite(9, LOW); //Brakes are turned off
@@ -15,4 +18,9 @@ void loop() {
   digitalWrite(9, HIGH) //Turn on brakes for Motor
   
   delay(1,000) //delay for 1,000 miliseconds(1 second) before repeating the loop
+  }
+  
+  else {
+  digitalWrite(9, HIGH); //brake if button is not pressed
+  }
 }
